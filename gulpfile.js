ff4afe4ac1486271e3f683adffd039e045d6d413
Yevhen Lakhno <webstyle.ugn@gmail.com>
2020-06-23 18:43:07 +0300

    finished project

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,15 @@ const gulp = require('gulp'),
       sass = require('gulp-sass'),
       rename = require('gulp-rename'),
       autoprefixer = require('gulp-autoprefixer'),
-      cleanCSS = require('gulp-clean-css');
+      cleanCSS = require('gulp-clean-css'),
+      imagemin = require('gulp-imagemin'),
+      htmlmin = require('gulp-htmlmin');
 
 // Static server
 gulp.task('server', function() {
     browserSync({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
 
@@ -25,12 +27,41 @@ gulp.task('styles', function() {
         }))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(sass|scss)", gulp.parallel('styles'));
+    gulp.watch("src/sass/**/*.+(sass|scss|css)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('html', function() {
+    return gulp.src("src/*.html")
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src("src/js/**/*.js")
+        .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task('fonts', function() {
+    return gulp.src("src/fonts/**/*")
+        .pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task('icons', function() {
+    return gulp.src("src/icons/**/*")
+        .pipe(gulp.dest("dist/icons"));
+});
+
+
+gulp.task('images', function() {
+    return gulp.src("src/img/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/img"));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'images'));
